19e8c3141f5fb19d64ff0149dba1353e8b46c39f
Akshat <akshatbhargavadeepa@gmail.com>
2022-10-31 15:26:58 +0530

    feat: integrated parser into the API

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
+const { fetchBankInfo } = require('./parser');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/banks', (req, res) => {
-    console.log(`received message from ${req.body}`);
-    console.log(req.body);
-    res.json({ data: [], error: null });
+app.get('/banks', async (req, res) => {
+    try {
+        const data = await fetchBankInfo();
+        res.json({ data, error: null });
+    } catch (err) {
+        res.json({ error: true, message: 'Something went wrong!' });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
